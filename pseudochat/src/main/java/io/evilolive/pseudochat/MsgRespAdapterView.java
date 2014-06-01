6e6042605fdebcb170800bd1b76e051ac0fa15bf
Jonathan Almeida <jonathan.almeida942@gmail.com>
2014-06-01 14:14:41 -0400

    Starting some implmentation of a private adapter Am I doing ths right?

--- pseudochat/src/main/java/io/evilolive/pseudochat/MsgRespAdapterView.java
+++ pseudochat/src/main/java/io/evilolive/pseudochat/MsgRespAdapterView.java
@@ -1,18 +1,69 @@
 package io.evilolive.pseudochat;
 
 import android.content.Context;
+import android.database.DataSetObserver;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 
 /**
  * Created by Jonathan Almeida on 11/03/14.
  */
 public class MsgRespAdapterView extends AdapterView {
+    private Adapter adapter = new Adapter() {
+        @Override
+        public void registerDataSetObserver(DataSetObserver dataSetObserver) {
+
+        }
+
+        @Override
+        public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
+
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+
+        @Override
+        public int getItemViewType(int i) {
+            return 0;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+    }
     public MsgRespAdapterView(Context context) {
         super(context);
-//        ArrayAdapter<Message> = new ArrayAdapter<Message>(context, )
     }
 
     @Override
