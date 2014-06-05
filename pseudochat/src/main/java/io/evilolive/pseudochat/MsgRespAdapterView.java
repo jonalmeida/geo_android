90770986ded2485fae97c2e6f285f48b4db7b51d
Jonathan Almeida <jonathan.almeida942@gmail.com>
2014-06-04 22:45:02 -0400

    AdapterView controls on user interaction  - TODO: addAndMeasureChild and positionItems

--- pseudochat/src/main/java/io/evilolive/pseudochat/MsgRespAdapterView.java
+++ pseudochat/src/main/java/io/evilolive/pseudochat/MsgRespAdapterView.java
@@ -11,57 +11,8 @@ import android.widget.AdapterView;
  * Created by Jonathan Almeida on 11/03/14.
  */
 public class MsgRespAdapterView extends AdapterView {
-    private Adapter adapter = new Adapter() {
-        @Override
-        public void registerDataSetObserver(DataSetObserver dataSetObserver) {
+    private Adapter adapter;
 
-        }
-
-        @Override
-        public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
-
-        }
-
-        @Override
-        public int getCount() {
-            return 0;
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return false;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            return null;
-        }
-
-        @Override
-        public int getItemViewType(int i) {
-            return 0;
-        }
-
-        @Override
-        public int getViewTypeCount() {
-            return 0;
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
-    }
     public MsgRespAdapterView(Context context) {
         super(context);
     }
@@ -83,6 +34,28 @@ public class MsgRespAdapterView extends AdapterView {
 
     @Override
     public void setSelection(int i) {
+        throw new UnsupportedOperationException("Not supported");
+    }
 
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+        if (adapter == null) {
+            return;
+        }
+
+        if (getChildCount() == 0) {
+            int position = 0;
+            int bottomEdge = 0;
+            while (bottomEdge < getHeight() && position < adapter.getCount()) {
+                View newBottomChild = adapter.getView(position, null, this);
+//                addAndMeasureChild(newBottomChild);
+                bottomEdge += newBottomChild.getMeasuredHeight();
+                position++;
+            }
+        }
+
+//        positionItems();
     }
 }
